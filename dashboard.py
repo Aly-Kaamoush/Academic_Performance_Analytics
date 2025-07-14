ac7833dba23cb5b803ff5027288150be30d7e905
Aly-Kaamoush <kaam0003@algonquinlive.com>
2025-07-14 13:57:40 -0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -6,6 +6,10 @@ Run with: streamlit run dashboard.py
 
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import plotly.graph_objects as go
 from datetime import datetime
 import os
 
